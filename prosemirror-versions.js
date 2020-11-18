b6da263b59b58205d90401c96185abe160195c34
Nandit <nanditmehra123@gmail.com>
2020-11-18 16:17:30 +0530

    undo last commit

--- prosemirror-versions.js
+++ prosemirror-versions.js
@@ -174,6 +174,7 @@ window.addEventListener('load', () => {
       "wss://signaling.yjs.dev",
       "wss://y-webrtc-signaling-eu.herokuapp.com",
       "wss://y-webrtc-signaling-us.herokuapp.com",
+      'ws://localhost:4444'
     ],
   });
   console.log('webrtc provider: ', provider)
